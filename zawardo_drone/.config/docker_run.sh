e947411b69cf4ba35caec3ace133263ae77d1fb3
Arthuino <vigouroux.art@gmail.com>
2024-11-05 01:35:31 +0100

    zawardo: RAM issue on ros_gz build fixed. Docker functional. Features still have to be tested

--- zawardo_drone/.config/docker_run.sh
+++ zawardo_drone/.config/docker_run.sh
@@ -7,6 +7,7 @@ docker run -it --rm \
             --privileged \
             --net host \
             --ipc host \
+            --device=/dev/dri/renderD128 \
             -e DISPLAY=$DISPLAY \
             -v /tmp/.X11-unix:/tmp/.X11-unix \
             -v /dev/dri/card0:/dev/dri/card0 \
