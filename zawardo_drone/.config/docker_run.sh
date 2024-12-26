0fa70755a8901c50def2c40da754ca4eedac4275
arthuino <art.vigouroux@gmail.com>
2024-12-26 17:55:22 +0100

    zawardo: docker volume fix

--- zawardo_drone/.config/docker_run.sh
+++ zawardo_drone/.config/docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+PROJECT_DIR=/home/arthuino/stymphale_project/zawardo_drone
 xhost +local:*
 
 docker run -it --rm \
@@ -11,7 +11,7 @@ docker run -it --rm \
             -e DISPLAY="$DISPLAY" \
             -v /tmp/.X11-unix:/tmp/.X11-unix \
             -v /dev/dri/card0:/dev/dri/card0 \
-            -v data:/home/ardupilotuser/ros2_ws/data \
+            -v $PROJECT_DIR/data:/home/ardupilotuser/ros2_ws/data:rw \
             arthuino/stymphale-zawardo:latest
             
 xhost -local:*
